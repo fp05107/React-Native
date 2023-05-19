3b42f78fcce5682cb86f2a7c12584e2c909c85a5
subham <andharalauri@gmail.com>
2023-05-19 16:38:36 +0530

    added

---
+++ ReactnativeControls/src/HomeScreen.jsx
@@ -0,0 +1,43 @@
+import { View, Text } from 'react-native'
+import React,{ useState} from 'react'
+import DropDownPicker from 'react-native-dropdown-picker'
+import { color } from 'react-native-elements/dist/helpers';
+
+const HomeScreen = () => {
+  const [isOpen,setIsOpen] = useState(false);
+  const [currentValue,setCurrentValue] = useState();
+  const items = [
+    {label:'Dancing',value:'dancing'},
+    {label:'Hiking',value:'hiking'},
+    {label:'Cricket',value:'cricket'},
+  ]
+  
+  return (
+    <View style={{padding:30}}>
+      <DropDownPicker items={items}
+      open={isOpen}
+      setOpen={() => setIsOpen(isOpen)}
+      value={currentValue}
+      setValue={(val) => setCurrentValue(val)}
+      maxHeight={200}
+      autoScroll
+      
+      placeholder='Select your hobby'
+      placeholderStyle={{color:'red',fontWeight:'bold'}}
+      showTickIcon={true}
+      showArrowIcon={true}
+      dropDownDirection='TOP'
+      theme='LIGHT'
+
+      multiple={true}
+      min={1}
+      max={4}
+      badgeColors={['red','blue','green']}
+      badgeDotColors={['white']}
+      badgeTextStyle={{color:'white'}}
+      />
+    </View>
+  )
+}
+
+export default HomeScreen
---
+++ ReactnativeControls/src/HomeStyle.jsx
@@ -0,0 +1,7 @@
+const {StyleSheet} = require('react-native/types');
+
+const styles = StyleSheet.create({
+  conatiner: {
+    backgroundColor: '#1D267D',
+  },
+});
