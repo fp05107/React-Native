ed2cfbf024a5e7dd876c3f2fbe0096e111a847ad
subham <andharalauri@gmail.com>
2023-06-25 10:14:07 +0530

    redux 1

--- TodoApp/App.jsx
+++ TodoApp/App.jsx
@@ -1,25 +1,12 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './src/pages/HomeScreen';
-import Details from './src/pages/Details';
-
-const Stack = createNativeStackNavigator();
-
-const App = () => {
+// App.js
+import { Provider } from 'react-redux';
+import Home from './src/screens/Home';
+import { store } from './src/redux/store';
+ 
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Overview'}}
-        />
-        <Stack.Screen name="Details" component={Details} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <Home />
+    </Provider>
   );
-};
-
-export default App;
+}
---
+++ TodoApp/src/pages/Todo.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Todo = () => {
+
+const myFun = () => {
+
+}
+  
+  return (
+    <View>
+      <Text>Todo</Text>
+    </View>
+  )
+}
+
+export default Todo
+
+const styles = StyleSheet.create({})
